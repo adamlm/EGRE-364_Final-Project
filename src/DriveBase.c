3afd8831d6fec3de6e9ea1fd4165ae0ffb802623
truslowpj <truslowpj@vcu.edu>
2016-12-07 01:08:17 -0500

    velocity control currently works

--- src/DriveBase.c
+++ src/DriveBase.c
@@ -10,9 +10,11 @@
 
 #include "DriveBase.h"
 #include "stdint.h"
+#include "math.h"
 
 
 static driveBase_t* driveBase;
+int right_speed = -20, left_speed = 20;
 
 void initDriveBase(driveBase_t* _driveBase) {  
   initStepperMotor(&(_driveBase->leftMotor), GPIOC, GPIOB, 5, 1);
@@ -27,7 +29,7 @@ static void initSyncTimer(driveBase_t* _driveBase) {
   // Init the timer
   RCC->APB1ENR1 |= RCC_APB1ENR1_TIM4EN; // Enable timer clock
   _driveBase->syncTimer = TIM4; // Set thw syncTimer to general timer 4
-  _driveBase->syncTimer->PSC = 400 - 1; // Want to run the counter at 2 kHz
+  _driveBase->syncTimer->PSC = 800 - 1; // Want to run the counter at 2 kHz
   _driveBase->syncTimer->ARR = 1000 - 1;  // Overrun event will trigger 1 kHz   
   
   // Enable interrupt for TIM4 and set its priority
@@ -47,11 +49,28 @@ void TIM4_IRQHandler(void) {
 
 void motorUpdate(void) {
   
-	uint8_t right_move = 1;
-	uint8_t left_move = 1;
+	uint8_t right_move =0;
+	uint8_t left_move = 0;
 	uint8_t right_dir = 1;
-	uint8_t left_dir = 1;
+	uint8_t left_dir = 0;
 	int i;
+	
+	static uint8_t right_count;
+	static uint8_t left_count;
+	
+	if (right_speed < 0) right_dir = 0;
+	if (left_speed < 0) left_dir = 1;
+	
+	
+	if (right_speed != 0 && right_count > 1000/abs(right_speed)) {
+		right_count = 0;
+		right_move = 1;
+	} else right_count++;
+
+	if (left_speed != 0 && left_count > 1000/abs(left_speed)) {
+		left_count = 0;
+		left_move = 1;
+	} else left_count++;
 	
 	if (left_move) {
 		set(&(driveBase->leftMotor),1, left_dir);
@@ -60,7 +79,7 @@ void motorUpdate(void) {
 		set(&(driveBase->rightMotor),1, right_dir);
 	}
 	
-	for(i=0; i<200; i++);
+	if (left_move || right_move) for(i=0; i<10; i++);
 
 	if (left_move) {
 		set(&(driveBase->leftMotor),0, left_dir);
